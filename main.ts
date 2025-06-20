32020a3e4c2f0903744e3fb6de28b3e5a720683f
rkky1144 <kwokkingyung913@gmail.com>
2025-06-20 11:19:36 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,15 @@
-SuperBit.Servo(0, 90)
+SuperBit.Servo(SuperBit.enServo.S4, 90)
 SuperBit.Servo(SuperBit.enServo.S1, 0)
-SuperBit.Servo(2, 90)
-SuperBit.Servo(3, 10)
+SuperBit.Servo(SuperBit.enServo.S2, 90)
+SuperBit.Servo(SuperBit.enServo.S3, 10)
 namespace servoCalculation {
 
     //%block = "clamp open $open $t3"
     export function clampOpen(open: boolean, t3: number) {
         if (open) {
-            SuperBit.Servo(3, 135 + t3)
+            SuperBit.Servo(SuperBit.enServo.S3, 135 + t3)
         } else {
-            SuperBit.Servo(3, 10 + t3)
+            SuperBit.Servo(SuperBit.enServo.S3, 10 + t3)
         }
     }
 
@@ -20,7 +20,7 @@ namespace servoCalculation {
                 let x = Math.map(index, 0, 100, 80, first_x)
                 let y = Math.map(index, 0, 100, 80, first_y)
                 SuperBit.Servo(SuperBit.enServo.S1, 90 - (Math.atan2(y, x) * 180 / 3.14159) - (Math.acos(Math.sqrt(x ** 2 + y ** 2) / 160) * 180 / 3.14159) + t1)
-                SuperBit.Servo(2, 90 + (Math.atan2(y, x) * 180 / 3.14159) - (Math.acos(Math.sqrt(x ** 2 + y ** 2) / 160) * 180 / 3.14159) + t2)
+                SuperBit.Servo(SuperBit.enServo.S2, 90 + (Math.atan2(y, x) * 180 / 3.14159) - (Math.acos(Math.sqrt(x ** 2 + y ** 2) / 160) * 180 / 3.14159) + t2)
                 basic.pause(5)
             }
         } else {
@@ -28,7 +28,7 @@ namespace servoCalculation {
                 let x2 = Math.map(index2, 0, 100, 80, first_x)
                 let y2 = Math.map(index2, 0, 100, 80, first_y)
                 SuperBit.Servo(SuperBit.enServo.S1, 90 - (Math.atan2(y2, x2) * 180 / 3.14159) - (Math.acos(Math.sqrt(x2 ** 2 + y2 ** 2) / 160) * 180 / 3.14159) + t1)
-                SuperBit.Servo(2, 90 + (Math.atan2(y2, x2) * 180 / 3.14159) - (Math.acos(Math.sqrt(x2 ** 2 + y2 ** 2) / 160) * 180 / 3.14159) + t2)
+                SuperBit.Servo(SuperBit.enServo.S2, 90 + (Math.atan2(y2, x2) * 180 / 3.14159) - (Math.acos(Math.sqrt(x2 ** 2 + y2 ** 2) / 160) * 180 / 3.14159) + t2)
                 basic.pause(5)
             }
         }
